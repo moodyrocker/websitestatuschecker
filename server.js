91ce639ad56eb4882f17eb6bf3239cda7731ccf6
moodyrocker <mudassar@aircandy.co.uk>
2025-03-22 08:47:59 +0000

    made it server side

---
+++ server.js
@@ -0,0 +1,27 @@
+// Simple script to start both client and server
+const { spawn } = require("child_process");
+
+// Start the server
+const server = spawn("node", ["server/index.js"], { stdio: "inherit" });
+
+// Start the client
+const client = spawn("npm", ["run", "dev"], { stdio: "inherit" });
+
+// Handle process termination
+process.on("SIGINT", () => {
+  server.kill();
+  client.kill();
+  process.exit();
+});
+
+server.on("close", (code) => {
+  console.log(`Server process exited with code ${code}`);
+  client.kill();
+  process.exit(code);
+});
+
+client.on("close", (code) => {
+  console.log(`Client process exited with code ${code}`);
+  server.kill();
+  process.exit(code);
+});
